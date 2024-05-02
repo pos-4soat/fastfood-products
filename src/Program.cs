621679291c7070b4ce5317fdb5251ed96a4b12ec
Corassin <44197980+Corassin@users.noreply.github.com>
2024-05-02 19:24:31 -0300

    Test run (#19)

--- src/Program.cs
+++ src/Program.cs
@@ -2,15 +2,8 @@ using fastfood_products.Data.Context;
 using fastfood_products.Handlers;
 using fastfood_products.IoC;
 using fastfood_products.Utils;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Reflection;
-using System.Text.Json;
-using System.Text.Unicode;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 IServiceCollection services = builder.Services;
@@ -23,65 +16,7 @@ configuration
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
     .AddEnvironmentVariables();
 
-logging.ClearProviders();
-logging.AddConsole();
-
-//services.AddValidatorsFromAssemblyContaining<CreateUserValidator>();
-//services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
-//services.AddFluentValidationRulesToSwagger();
-
-services
-    .AddControllers(o =>
-    {
-        //o.Filters.Add(typeof(ValidationAttributeHandler));
-        o.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
-    })
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
-    });
-
-services
-    .Configure<ApiBehaviorOptions>(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
-
-var conStr = builder.Configuration.GetConnectionString("SqlServerConnection");
-if (string.IsNullOrWhiteSpace(conStr))
-    throw new InvalidOperationException(
-        $"Could not find a connection string named 'SqlServerConnection'.");
-services
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen(setup =>
-    {
-        setup.SwaggerDoc("v1",
-            new Microsoft.OpenApi.Models.OpenApiInfo
-            {
-                Title = "FastFood Products",
-                Version = "v1",
-                Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                {
-                    //TODO - Add e-mail and name
-                    Email = "",
-                    Name = "",
-                }
-            });
-
-        var filePath = Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
-        setup.IncludeXmlComments(filePath);
-    })
-    .AddApiVersioning(options =>
-    {
-        options.ReportApiVersions = true;
-        options.AssumeDefaultVersionWhenUnspecified = true;
-        options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
-    })
-    .AddHealthChecks()
-    .AddCheck<SimpleHealthCheck>(
-        "HealthCheck",
-        tags: new[] { "HealthCheck" })
-    .AddSqlServer(conStr);
+logging.ConfigureLogging();
 
 services.RegisterServices(configuration);
 
@@ -98,26 +33,22 @@ app.UseRouting();
 app.UseAuthorization();
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 
-app.UseEndpoints(endpoints =>
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
 {
-    app.MapHealthChecks("health/readiness", new HealthCheckOptions
-    {
-        Predicate = healthCheck => healthCheck.Tags.Contains("HealthCheck"),
-    });
-
-    app.MapHealthChecks("health/liveness", new HealthCheckOptions
-    {
-        Predicate = healthCheck => healthCheck.Tags.Contains("HealthCheck"),
-    });
-
-    endpoints.MapControllers();
+    Predicate = _ => _.Tags.Contains("ready"),
+    ResponseWriter = HealthCheckResponseWriter.WriteResponse
 });
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = _ => _.Tags.Contains("live"),
+    ResponseWriter = HealthCheckResponseWriter.WriteResponse
+});
+
+app.MapDefaultControllerRoute();
+
+app.MapControllers();
+
 app.UseHttpsRedirection();
-
-app.MapControllerRoute(name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
--- src/Utils/SimpleHealthCheck.cs
+++ src/Utils/SimpleHealthCheck.cs
@@ -4,16 +4,9 @@ namespace fastfood_products.Utils
 {
     public class SimpleHealthCheck : IHealthCheck
     {
-        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
-            try
-            {
-                return await Task.FromResult(HealthCheckResult.Healthy());
-            }
-            catch (Exception ex)
-            {
-                return await Task.FromResult(HealthCheckResult.Unhealthy(exception: ex));
-            }
+            return Task.FromResult(HealthCheckResult.Healthy());
         }
     }
 }
