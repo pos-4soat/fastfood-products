48a5528bdae39367583230ef4ac862dc56bd268a
Corassin <44197980+Corassin@users.noreply.github.com>
2024-05-02 10:45:43 -0300

    Test run (#8)
    * update-database
    
    * encode-rds
    
    * update-dataschema
    
    * schema-add

--- src/Data/Migrations/20240502132041_update-schema.Designer.cs
+++ src/Data/Migrations/20240502132041_update-schema.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using fastfood_products.Data.Context;
 
@@ -10,9 +11,11 @@ using fastfood_products.Data.Context;
 namespace fastfood_products.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240502132041_update-schema")]
+    partial class updateschema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
