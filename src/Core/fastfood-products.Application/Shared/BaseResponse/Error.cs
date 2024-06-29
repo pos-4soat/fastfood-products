2f48f1b322aa2fd9a2ba08dea50768a06ee373bd
Marcello Corassin <marcellocorassin@hotmail.com>
2024-06-29 12:04:13 -0300

    update error

--- src/Core/fastfood-products.Application/Shared/BaseResponse/Error.cs
+++ src/Core/fastfood-products.Application/Shared/BaseResponse/Error.cs
@@ -6,9 +6,7 @@ namespace fastfood_products.Application.Shared.BaseResponse;
 /// <param name="Message"> Mensagens de erro. </param>
 public sealed record Error(string ErrorCode, string Message)
 {
-    public Error(string errorCode) : this(errorCode, ErrorMessages.ErrorMessageList[errorCode])
+    public Error(string errorCode) : this(errorCode, ErrorMessages.ErrorMessageList.TryGetValue(errorCode, out string message) ? message : ErrorMessages.ErrorMessageList["PIE999"])
     {
-        if (string.IsNullOrEmpty(Message))
-            Message = ErrorMessages.ErrorMessageList["PIE999"];
     }
 }
